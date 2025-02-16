f11645aa674a15935277756838cfb90e33b846f8
Vijay <vijaybodem17@gmail.com>
2025-02-16 22:12:52 +0530

    server setup and first route

--- api/index.js
+++ api/index.js
@@ -1,1 +1,13 @@
-console.log("helo");
+import express from "express";
+
+const app = express();
+
+console.log("first");
+
+app.use("/api/test", (req, res) => {
+  res.send("Its working ");
+});
+
+app.listen(2001, () => {
+  console.log("Server is running...");
+});
