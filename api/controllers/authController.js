396ab2203ff84edeefa914585118378f5aece595
Vijay <vijaybodem17@gmail.com>
2025-03-21 22:10:54 +0530

    login - jwt - logout

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,5 +1,7 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import prisma from "../libs/prisma.js";
+
 export const register = async (req, res) => {
   const { username, email, password } = req.body;
 
@@ -27,10 +29,51 @@ export const register = async (req, res) => {
   }
 };
 
-export const login = (req, res) => {
-  // db operations
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+  console.log(username, password);
+  try {
+    // if the user exists or not
+    const user = await prisma.user.findUnique({
+      where: { username },
+    });
+
+    console.log(user);
+
+    if (!user) return res.status(401).json({ message: "Invalid Credentials" });
+
+    // Check if the password is correct or not
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+
+    if (!isValidPassword)
+      return res.status(401).json({ message: "Invalid Credentials" });
+
+    //Generate cookie token and send to the user
+    const age = 1000 * 60 * 60 * 24 * 7;
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+      },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: age }
+    );
+
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        // secure : true
+        maxAge: age,
+      })
+      .status(200)
+      .json({ message: "Login Successfull" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Failed to login" });
+  }
 };
 
 export const logout = (req, res) => {
-  // db operations
+  res.clearCookie("token").status(200).json({ message: "Logout Successfull" });
 };
--- api/index.js
+++ api/index.js
@@ -1,10 +1,12 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import authRoute from "./routes/authRoute.js";
 import postRoute from "./routes/postRoute.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoute);
 app.use("/api/post", postRoute);
