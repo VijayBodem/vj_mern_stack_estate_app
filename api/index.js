f49da2104bca3d6ad0a513fa6e9d64b960c0917e
Vijay <vijaybodem17@gmail.com>
2025-03-21 22:52:45 +0530

    client url added

--- api/index.js
+++ api/index.js
@@ -1,10 +1,11 @@
 import express from "express";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import authRoute from "./routes/authRoute.js";
 import postRoute from "./routes/postRoute.js";
 
 const app = express();
-
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
