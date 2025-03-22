42dbf5d3c91a343aef65f69af6d091959631ad50
Vijay <vijaybodem17@gmail.com>
2025-03-22 10:33:36 +0530

    userinfo sending after successfull login

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -60,6 +60,7 @@ export const login = async (req, res) => {
       { expiresIn: age }
     );
 
+    const { password: userPassword, ...userInfo } = user;
     res
       .cookie("token", token, {
         httpOnly: true,
@@ -67,7 +68,7 @@ export const login = async (req, res) => {
         maxAge: age,
       })
       .status(200)
-      .json({ message: "Login Successfull" });
+      .json(userInfo);
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Failed to login" });
