bd23642f76794e0ba06cbada0c413fd085907872
Vijay <vijaybodem17@gmail.com>
2025-03-29 14:02:43 +0530

    user controller, user route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import authRoute from "./routes/authRoute.js";
 import postRoute from "./routes/postRoute.js";
 import testtRoute from "./routes/testRoute.js";
+import userRoute from "./routes/userRoute.js";
 
 const app = express();
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
@@ -13,6 +14,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoute);
 app.use("/api/post", postRoute);
 app.use("/api/test", testtRoute);
+app.use("/api/user", userRoute);
 
 const port = 2002;
 
---
+++ api/routes/userRoute.js
@@ -0,0 +1,20 @@
+import express from "express";
+import {
+  deleteUser,
+  getUser,
+  getUsers,
+  updateUser,
+} from "../controllers/userController.js";
+import { verifyToken } from "../midddleware/verifyToken.js";
+
+const router = express.Router();
+
+router.get("/", getUsers);
+
+router.get("/:id", verifyToken, getUser);
+
+router.put("/:id", verifyToken, updateUser);
+
+router.delete("/:id", verifyToken, deleteUser);
+
+export default router;
